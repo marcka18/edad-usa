4d9c348bcec80c95ea51b5f99b37f2558a1cc288
Marck Alvarez <>
2021-04-27 21:33:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let edad  = prompt("ingrese la edad");
+if(edad >="21"){
+		console.log ("Usted es Mayor de edad en USA");
+	}else if(edad <"21"){
+		console.log("Usted es menor de edad en USA ");
+	}
